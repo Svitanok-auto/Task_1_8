fea579e404a7a8414754239a117c6d6c5401f585
Sveta <bogatovasvetlanau@gmail.com>
2021-05-31 12:56:02 +0300

    feature is developed

---
+++ ConsoleApp8/Models/CustomerModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8.Models
+{
+    public class CustomerModel
+    {
+        [JsonProperty("customer")]
+        public string Customer { get; set; }
+        [JsonProperty("shipment")]
+        public List<ShipmentModel> Shipments { get; set; }
+        public Guid ID { get; set; }
+    }
+}
---
+++ ConsoleApp8/Models/CustomersModel.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp8.Models
+{
+    public class CustomersModel
+    {
+        [JsonProperty("customers")]
+        public List<CustomerModel> Customers { get; set; }
+    }
+}
